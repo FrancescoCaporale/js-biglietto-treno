10f0ef8c192728bec01833cce8da5f260eb61da8
FrancescoCaporale <frenk.caporale@gmail.com>
2025-12-01 16:51:14 +0100

    Aggiunto Calcolo del prezzo e Output

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
 
 //Input utente//
-const userAge = Number(prompt("Quanti anni hai?"))
+const userAge = Number(prompt("Quanti anni hai?"));
+const userKm = Number(prompt("Inserisci i km da percorrere:"));
 
-console.log("Età:", userAge)
+console.log("Età:", userAge);
+console.log("Km:", userKm);
 
 //Apllicazione sconti//
 if (userAge<18) {
-    console.log("Utente Minorenne - Sconto 20%")
+    console.log("Utente Minorenne - Sconto 20%");
 } else if (userAge >= 65) {
-    console.log("Utente Senior - Sconto 40%")
+    console.log("Utente Senior - Sconto 40%");
 } else {
-    console.log("Nessuno Sconto Applicato")
+    console.log("Nessuno Sconto Applicato");
 }
+
+//CAlcolo prezzo//
+let ticketPrice = userKm * 0.21;
+console.log("Prezzo base:", ticketPrice);
+
+//Output//
+console.log("Prezzo finale:", finalPrice + "€");
