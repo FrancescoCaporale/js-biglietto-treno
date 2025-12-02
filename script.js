30eab20a43ebd1aa08c461111c4a94564b902b76
FrancescoCaporale <frenk.caporale@gmail.com>
2025-12-02 09:29:11 +0100

    fix: prezzo e decimale

--- script.js
+++ script.js
@@ -7,18 +7,22 @@ const userKm = Number(prompt("Inserisci i km da percorrere:"));
 console.log("Età:", userAge);
 console.log("Km:", userKm);
 
+//CAlcolo prezzo//
+let ticketPrice = userKm * 0.21;
+console.log("Prezzo base:", ticketPrice);
+
 //Apllicazione sconti//
 if (userAge<18) {
+     ticketPrice = ticketPrice * 0.8;
     console.log("Utente Minorenne - Sconto 20%");
 } else if (userAge >= 65) {
+     ticketPrice = ticketPrice * 0.6;
     console.log("Utente Senior - Sconto 40%");
 } else {
     console.log("Nessuno Sconto Applicato");
 }
 
-//CAlcolo prezzo//
-let ticketPrice = userKm * 0.21;
-console.log("Prezzo base:", ticketPrice);
+const finalPrice = ticketPrice.toFixed(2)
 
 //Output//
-console.log("Prezzo finale:", finalPrice + "€");
+console.log("Prezzo finale:", ticketPrice + "€");
